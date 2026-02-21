c38e223de04b440faa1c8db463cfca465eb9538e
davoudn <dd.nasr@gmail.com>
2026-02-21 18:56:34 +0330

    build removed

--- codes/Parser/run.sh
+++
@@ -1,5 +0,0 @@
-# rm -rf generated
-# antlr4 -Dlanguage=Cpp -no-listener -visitor -o generated/ -package antlrcpptest TLexer.g4 TParser.g4
-rm a.out
-g++ -I /usr/local/include/antlr4-runtime main.cpp generated/*.cpp -L /usr/local/lib/libantlr4-runtime.* -Wall -Wextra --std=c++17
-
